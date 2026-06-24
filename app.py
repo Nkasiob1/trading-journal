38ed35df6d68caf8938dfd9cc46f824c6708dfe0
Precious Ukaegbu <preciousukaegbu72@gmail.com>
2026-06-24 02:15:02 +0100

    Session 1: Initialize Flask app for GOAT trading journal

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+# Create an instance of the Flask app and assign it to a variable called app
+# __name__ tells Flask where to look for files related to this app
+app = Flask (__name__)
+# This is a decorator — it tells Flask that when someone visits '/' (the homepage)
+# it should run the function directly below it
+@app.route('/')
+# This is the function that runs when someone visits the homepage
+def home():
+     # This is what gets sent back to the browser when the homepage is visited
+     return 'GOAT Trading Journal is running'
+# This means: only run the app if this file is being run directly
+# not if it's being imported by another file
+if __name__=='__main_-':
+      # Start the Flask web server with debug=True
+    # debug=True means the server restarts automatically when you save changes
+    # and shows detailed error messages in the browser
+    app.run(debug=True)
